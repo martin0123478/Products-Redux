a8523c88d2b40d501223226c659230819434a3c3
martin0123478 <martin_agui97@outlook.es>
2023-05-16 13:53:09 -0600

    feat:create form to edit, create and table to list and db

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 const EditarProducto = () => {
   return (
-    <div>
-      EditarProducto
+     <div className='row justify-content-center'>
+        <div className='col-md-8'>
+            <div className='card'>
+                <div className='card-body'>
+                    <h2 className='text-center mb-4 font-weigth-bold'>
+                        Editar Prducto
+                    </h2>
+
+                    <form>
+                        <div className='form-group'>
+                            <label>Nombre Producto</label>
+                            <input className='form-control' type="text"
+                            placeholder='Nombre Producto'
+                            name='nombre' />
+                        </div>
+                        <div className='form-group'>
+                            <label>Precio Producto</label>
+                            <input className='form-control' type="number"
+                            placeholder='Precio Producto'
+                            name='precio' />
+                        </div>
+
+                        <button type='submit' className='btn btn-primary font-weight-bold
+                        text-uppercase d-block w-100'>
+                            Guardar Cambios
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
