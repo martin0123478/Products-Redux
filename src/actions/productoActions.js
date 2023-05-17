2444ae16a4de4aca3c9516ddea0053ede48b22c8
martin0123478 <martin_agui97@outlook.es>
2023-05-17 12:20:15 -0600

    feat:pass info to action

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -5,8 +5,8 @@ import {
 } from "../types/index";
 
 //Crear nuevos productos
-export function crearNuevoProductoAction() {
+export function crearNuevoProductoAction(producto) {
   return () => {
-    console.log("desde action");
+    console.log(producto);
   };
 }
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -3,17 +3,22 @@ import { useDispatch,useSelector } from 'react-redux'
 //Actions
 import {crearNuevoProductoAction} from '../actions/productoActions'
 const NuevoProducto = () => {
+    //state componente
+    const [nombre,setNombre] = useState('')
+    const [precio,setPrecio] = useState(0)
    //utilizar useDispatch y crea una funcion
     const dispatch = useDispatch()
     //mandar a llamar action de productoActions
-    const agregarProducto =()=> dispatch(crearNuevoProductoAction())
+    const agregarProducto =producto=> dispatch(crearNuevoProductoAction(producto))
     
     const submitNuevoProducto = e =>{
         e.preventDefault()
         //validar form
-
+        if(nombre.trim() === '' || precio <0){
+            return
+        }
         //crear nuevo producto
-        agregarProducto()
+        agregarProducto({nombre,precio})
     }
   return (
     <div className='row justify-content-center'>
@@ -30,13 +35,17 @@ const NuevoProducto = () => {
                             <label>Nombre Producto</label>
                             <input className='form-control' type="text"
                             placeholder='Nombre Producto'
-                            name='nombre' />
+                            name='nombre'
+                            value={nombre}
+                            onChange={e =>setNombre(e.target.value)} />
                         </div>
                         <div className='form-group'>
                             <label>Precio Producto</label>
                             <input className='form-control' type="number"
                             placeholder='Precio Producto'
-                            name='precio' />
+                            name='precio'
+                            value={precio}
+                            onChange={e => setPrecio(Number(e.target.value))} />
                         </div>
 
                         <button type='submit' className='btn btn-primary font-weight-bold
