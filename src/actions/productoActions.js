1a092ab0712723fdb9742e3b567d8860db520488
martin0123478 <martin_agui97@outlook.es>
2023-05-17 16:14:45 -0600

    feat:Add products in API

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,14 +3,18 @@ import {
   AGREGAR_PRODUCTO_EXITO,
   AGREGAR_PRODUCTO_ERROR,
 } from "../types/index";
-
+import clienteAxios from "../config/axios";
 //Crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(agregarProducto());
     try {
+      //insertar en API
+      await clienteAxios.post("/hols", producto);
+
       dispatch(agregarProductoExito(producto));
     } catch (error) {
+      console.log(error);
       dispatch(agregarProductoError(true));
     }
   };
